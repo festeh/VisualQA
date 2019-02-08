7d38c74605802e7992a4af34d83a2c88ea4df864
dima <dimich3d@ya.ru>
2019-02-08 22:41:22 +0300

    Start Ignite integration

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import logging
 import pickle
+
+import numpy
 import torch
 from pathlib import Path
 
+from ignite.engine import create_supervised_trainer, create_supervised_evaluator, Events
+from ignite.metrics import Loss, Accuracy
 from torch.nn import CrossEntropyLoss
 from torch.optim import Adam
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
 from src.models.baseline import BaselineModel
-from src.utils.load_all_data import get_data, VisualQADataset
+from src.utils.load_all_data import get_data, VisualQADataset, VisualQAValidationDataset
 
 config = {
     "raw_data_path": "/data/",
@@ -23,7 +27,13 @@ logging.basicConfig(level=logging.INFO, format=LOG_FORMAT, datefmt='%H:%M:%S')
 data, ans_vocab = get_data(Path(config["raw_data_path"]), Path(config['preprocessed_data_path']),
                            parts=["train", "val"])
 train_dataset = VisualQADataset(*data['train'], answer_vocabulary=ans_vocab)
+
+train_dataset.qa = train_dataset.qa[:1000]
+
+val_dataset = VisualQAValidationDataset(*data['val'], answer_vocabulary=ans_vocab)
+
 train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=128, shuffle=False)
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 logging.info(f"Using device: {device}")
@@ -32,18 +42,61 @@ model = BaselineModel(
     image_emb_size=4096,
     n_classes=1000,
     h_size=300,
-    emb_path="/data/preprocessed/embeddings.pkl")
-model.to(device)
+    emb_path="/data/preprocessed/embeddings.pkl").to(device)
 
 optimizer = Adam(model.parameters(), 1e-3)
-loss_fn = CrossEntropyLoss()
-#
-for questions, answers, image_embs in tqdm(train_loader):
-    optimizer.zero_grad()
-    logits = model(questions, image_embs.to(device))
-    loss = loss_fn(logits, answers.to(device))
-    loss.backward()
-    optimizer.step()
-    print(loss.item())
+loss = CrossEntropyLoss()
 
-torch.save(model.state_dict(), "models/baseline.pth")
+trainer = create_supervised_trainer(model, optimizer, loss, device=device)
+evaluator = create_supervised_evaluator(model,
+                                        metrics={'accuracy': Accuracy(),
+                                                 'nll': Loss(loss)
+                                                 }, device=device)
+
+@trainer.on(Events.ITERATION_COMPLETED)
+def log_training_loss(trainer):
+    print("Epoch[{}] Loss: {:.2f}".format(trainer.state.epoch, trainer.state.output))
+
+@trainer.on(Events.EPOCH_COMPLETED)
+def log_training_results(trainer):
+    evaluator.run(train_loader)
+    metrics = evaluator.state.metrics
+    print("Training Results - Epoch: {}  Avg accuracy: {:.2f} Avg loss: {:.2f}"
+          .format(trainer.state.epoch, metrics['accuracy'], metrics['nll']))
+
+@trainer.on(Events.EPOCH_COMPLETED)
+def log_validation_results(trainer):
+    evaluator.run(val_loader)
+    metrics = evaluator.state.metrics
+    print("Validation Results - Epoch: {}  Avg accuracy: {:.2f} Avg loss: {:.2f}"
+          .format(trainer.state.epoch, metrics['accuracy'], metrics['nll']))
+
+
+trainer.run(train_loader, max_epochs=100)
+
+
+# #
+# for iter, (questions, answers, image_embs) in tqdm(enumerate(train_loader)):
+#     optimizer.zero_grad()
+#     logits = model(questions, image_embs.to(device))
+#     loss = loss_fn(logits, answers.to(device))
+#     loss.backward()
+#     optimizer.step()
+#     print(loss.item())
+#
+#     if iter % 100 == 0:
+#         accs = []
+#         for val_iter, (val_questions, val_answers, val_image_embs) in tqdm(enumerate(val_loader, 1)):
+#             with torch.no_grad():
+#                 logits = model(val_questions, val_image_embs.to(device))
+#                 preds = logits.argmax(1).cpu()
+#             n_correct_for_question = (val_answers == preds[:, None]).sum(1)
+#             acc = n_correct_for_question.type(torch.float32) / 3
+#             acc.clamp_(0, 1)
+#             accs.extend(acc.numpy())
+#
+#             if val_iter % 100 == 0:
+#                 print("Mean validation acuracy:", numpy.mean(accs))
+#                 break
+#
+# torch.save(model.state_dict(), "models/baseline.pth")
