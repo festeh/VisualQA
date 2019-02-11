a3bbbffd2f23528e97cf0274df71ccc941a3014d
dima <dimich3d@ya.ru>
2019-02-11 23:52:47 +0300

    Add eval accuracy metric

---
+++ src/metrics.py
@@ -0,0 +1,35 @@
+import torch
+
+from ignite.metrics.accuracy import _BaseClassification
+
+
+class VisualQAAccuracy(_BaseClassification):
+    def reset(self):
+        self._num_correct = 0
+        self._num_examples = 0
+
+    def update(self, output):
+        y_pred, y = output
+
+        assert y.size(1) == 10
+
+        # y_pred, y = self._check_shape(output)
+        # self._check_type((y_pred, y))
+
+        indices = torch.argmax(y_pred, dim=1).view(-1, 1)
+
+        # if self._type == "binary":
+        #     indices = torch.round(y_pred).type(y.type())
+        # elif self._type == "multiclass":
+        #     indices = (y_pred, dim=1)[1]
+        #
+        correct = torch.eq(indices, y).sum(1).type(torch.float32) / 3
+        correct = torch.clamp(correct, 0, 1)
+
+        self._num_correct += torch.sum(correct).item()
+        self._num_examples += correct.shape[0]
+
+    def compute(self):
+        if self._num_examples == 0:
+            raise ValueError('Accuracy must have at least one example before it can be computed')
+        return self._num_correct / self._num_examples
