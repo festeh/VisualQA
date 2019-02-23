6bc008f488fecb232173005371d9a7135fa67a82
dima <dimich3d@ya.ru>
2019-02-23 17:39:28 +0300

    Specify --overwrite-device in dvc command

--- src/utils/dvc.py
+++ src/utils/dvc.py
@@ -1,27 +1,25 @@
-
+import logging
 import subprocess, os
-
-# TODO: nasty, use env variable?
 from typing import Dict, Union, List
 
+# TODO: nasty, use env variable?
 CONDA_PATH = "/home/dima/miniconda3/envs/visualqa/"
 
 
 def dvc_run(
         script_path: str,
         script_arguments: Dict,
-        output_deps=Union[str, List[str]]):
-    arguments_str = " ".join(f"--{arg_name}={arg_value}" for arg_name, arg_value in script_arguments)
+        output_deps: Union[str, List[str]],
+        dvc_file_name=None):
+    arguments = " ".join(f"--{arg_name}={arg_value}" for arg_name, arg_value in script_arguments.items())
     if isinstance(output_deps, str):
         output_deps = [output_deps]
-
-    output_deps_str = [f"-o {dep}" for dep in output_deps]
-    command = f"dvc run {output_deps_str} -d {script_path} python {script_path} {arguments_str}"
-    return command
+    output_deps = ' '.join([f'-o {dep}' for dep in output_deps])
+    dvc_file_name = f'-f {dvc_file_name}' if dvc_file_name is not None else ''
+    command = f"dvc run --overwrite-dvcfile {dvc_file_name} {output_deps} -d {script_path} python {script_path} {arguments}"
+    logging.info(f"Executing: {command}")
+    execute_dvc_command(command)
 
 
 def execute_dvc_command(dvc_command):
     subprocess.run(f'bash -c "conda run -p {CONDA_PATH} {dvc_command}"', shell=True)
-
-
-
