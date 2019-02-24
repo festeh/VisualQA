f36e293e370ebf9f281a777ecaff77f3db1fea49
dima <dimich3d@ya.ru>
2019-02-24 18:46:12 +0300

    more flexible init_config

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -14,6 +14,10 @@ def filter_config(config: Dict, function: Callable):
     return {k: v for k, v in config.items() if k in func_args}
 
 
-def init_config(config_part: str, target_function: Callable):
-    data_params = Params.from_file("config.jsonnet").pop(config_part).as_dict()
-    return filter_config(data_params, target_function)
+def init_config(config_part: str = None, target_function: Callable = None):
+    data_params = Params.from_file("config.jsonnet")
+    if config_part is not None:
+        data_params = data_params.pop(config_part)
+    if target_function is None:
+        return data_params
+    return filter_config(data_params.as_dict(), target_function)
