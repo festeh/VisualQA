163fb871d6974b9acf11658886a290240040f3f9
dima <dimich3d@ya.ru>
2019-02-24 14:03:26 +0300

    Support input deps, omit arguments

--- src/utils/dvc.py
+++ src/utils/dvc.py
@@ -8,15 +8,25 @@ CONDA_PATH = "/home/dima/miniconda3/envs/visualqa/"
 
 def dvc_run(
         script_path: str,
-        script_arguments: Dict,
         output_deps: Union[str, List[str]],
+        input_deps: Union[str, List[str]] = "",
+        script_arguments=None,
         dvc_file_name=None):
-    arguments = " ".join(f"--{arg_name}={arg_value}" for arg_name, arg_value in script_arguments.items())
+    if script_arguments is not None:
+        script_arguments = " ".join(f"--{arg_name}={arg_value}" for arg_name, arg_value in script_arguments.items())
+    else:
+        script_arguments = ""
     if isinstance(output_deps, str):
         output_deps = [output_deps]
     output_deps = ' '.join([f'-o {dep}' for dep in output_deps])
+    if input_deps is None:
+        input_deps = ""
+    elif isinstance(input_deps, str):
+        input_deps = [input_deps]
+    input_deps = ' '.join([f'-d {dep}' for dep in input_deps])
     dvc_file_name = f'-f {dvc_file_name}' if dvc_file_name is not None else ''
-    command = f"dvc run --overwrite-dvcfile {dvc_file_name} {output_deps} -d {script_path} python {script_path} {arguments}"
+    command = f"dvc run --overwrite-dvcfile {dvc_file_name} {output_deps} -d {script_path} {input_deps} " \
+        f"python {script_path} {script_arguments}"
     logging.info(f"Executing: {command}")
     execute_dvc_command(command)
 
