4a56e5ea7db98c2402468618d9439c01149b5a64
dima <dimich3d@ya.ru>
2019-02-24 14:13:10 +0300

    skip arguments pass

--- pipeline/preprocess_qa.py
+++ pipeline/preprocess_qa.py
@@ -6,6 +6,5 @@ from src.utils.dvc import dvc_run
 
 data_params = experiment_config.pop("data")
 dvc_run("src/utils/load_qa.py",
-        script_arguments=data_params.as_dict(),
         dvc_file_name=f"{Path(__file__).stem}.dvc",
-        output_deps=data_params.get("qa_saving_dir"))
+        output_deps=[data_params.get("train_qa_result_file"), data_params.get("val_qa_result_file")])
