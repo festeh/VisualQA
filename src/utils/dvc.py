29f4bacb00c05d1e8f6ae7cdb2d303b39591cd3c
dima <dimich3d@ya.ru>
2019-02-24 18:44:57 +0300

    bug fix with empty deps

--- src/utils/dvc.py
+++ src/utils/dvc.py
@@ -9,19 +9,19 @@ CONDA_PATH = "/home/dima/miniconda3/envs/visualqa/"
 def dvc_run(
         script_path: str,
         output_deps: Union[str, List[str]],
-        input_deps: Union[str, List[str]] = "",
+        input_deps: Union[str, List[str]] = '',
         script_arguments=None,
         dvc_file_name=None):
     if script_arguments is not None:
         script_arguments = " ".join(f"--{arg_name}={arg_value}" for arg_name, arg_value in script_arguments.items())
     else:
         script_arguments = ""
-    if isinstance(output_deps, str):
+    if output_deps and isinstance(output_deps, str):
         output_deps = [output_deps]
     output_deps = ' '.join([f'-o {dep}' for dep in output_deps])
     if input_deps is None:
         input_deps = ""
-    elif isinstance(input_deps, str):
+    elif input_deps and isinstance(input_deps, str):
         input_deps = [input_deps]
     input_deps = ' '.join([f'-d {dep}' for dep in input_deps])
     dvc_file_name = f'-f {dvc_file_name}' if dvc_file_name is not None else ''
