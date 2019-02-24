6e55e9dff28858b370a05df01e1fec8de25e0747
dima <dimich3d@ya.ru>
2019-02-24 14:17:22 +0300

    rewrite image loading (from zero)

--- src/utils/load_images.py
+++ src/utils/load_images.py
@@ -1,19 +1,33 @@
+import json
+import logging
 from logging import info
 from pathlib import Path
 
+import h5py
 import numpy as np
 import torch
 from einops import rearrange
-from fastprogress import progress_bar
 from skimage.io import imread
 from skimage.transform import resize as imresize
 from torch.utils.data import DataLoader, Dataset
 from torchvision.models import vgg16
+from tqdm import tqdm
+
+from src.utils.helpers import create_parent_dir_if_not_exists, init_config
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
-# def read_img_batch(img_paths):
-#     imgs = [read_image(img_paths[i], resize=True) for i in range(len(img_paths))]
-#     return np.stack(imgs, axis=0)
+def read_image(img_path, resize=False):
+    image = imread(img_path)
+
+    if len(image.shape) != 3:
+        image = np.stack([image, image, image], axis=2)
+
+    if resize:
+        return imresize(image, (224, 224), mode='reflect', anti_aliasing=True).astype(np.float32)
+    return image
 
 
 def process_batch(model, imgs, processed, already_tensor=False):
@@ -48,25 +62,43 @@ def load_pretrained_feature_extractor():
     return model
 
 
-def preprocess_images(imgs_dir, n_jobs=4, batch_size=128):
-    info("Start preprocessing images")
+def preprocess_images(
+        train_images, val_images,
+        train_images_result_file, val_images_result_file,
+        train_filenames_result_file, val_filenames_result_file,
+        n_jobs=4,
+        batch_size=128):
+    info("Start preprocessing raw images")
     model = load_pretrained_feature_extractor()
-    files = sorted(Path(imgs_dir).iterdir())
-    preprocessed = []
-    imgs_dataset = RawImagesDataset(files)
-    dataloader = DataLoader(imgs_dataset, batch_size=batch_size,
-                            shuffle=False, num_workers=n_jobs)
-    for batch in progress_bar(dataloader):
-        process_batch(model, batch.cuda(), preprocessed, True)
-    return [x.stem for x in files], np.concatenate(preprocessed)
+
+    def preprocess_dataset(images_path, filenames_saving_path, images_saving_path):
+        files = sorted(Path(images_path).iterdir())
+        preprocessed = []
+        imgs_dataset = RawImagesDataset(files)
+        dataloader = DataLoader(imgs_dataset, batch_size=batch_size,
+                                shuffle=False, num_workers=n_jobs)
+        for batch in tqdm(dataloader):
+            process_batch(model, batch.cuda(), preprocessed, True)
+        filenames = [x.stem for x in files]
+        images = np.concatenate(preprocessed)
+
+        filenames_saving_path = Path(filenames_saving_path)
+        create_parent_dir_if_not_exists(filenames_saving_path)
+        with filenames_saving_path.open("w"):
+            json.dump(filenames, filenames_saving_path.open("w"))
+
+        images_saving_path = Path(images_saving_path)
+        create_parent_dir_if_not_exists(images_saving_path)
+        with h5py.File(images_saving_path, "w") as f:
+            f.create_dataset("images", data=images, dtype='float32')
+        return filenames, images
+
+    preprocess_dataset(train_images, filenames_saving_path=train_filenames_result_file,
+                       images_saving_path=train_images_result_file)
+    preprocess_dataset(val_images, filenames_saving_path=val_filenames_result_file,
+                       images_saving_path=val_images_result_file)
+    info("Processed raw images!")
 
 
-def read_image(img_path, resize=False):
-    image = imread(img_path)
-
-    if len(image.shape) != 3:
-        image = np.stack([image, image, image], axis=2)
-
-    if resize:
-        return imresize(image, (224, 224), mode='reflect', anti_aliasing=True).astype(np.float32)
-    return image
+if __name__ == "__main__":
+    preprocess_images(**init_config("data", preprocess_images))
