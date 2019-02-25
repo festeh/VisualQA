3ce0e9b1db9ab5a80909034bd50fed166c03c1ee
dima <dimich3d@ya.ru>
2019-02-25 15:30:39 +0300

    pipeline fixes

--- pipeline/create_vocab.py
+++ pipeline/create_vocab.py
@@ -4,10 +4,10 @@ from pathlib import Path
 from pipeline.init import experiment_config
 from src.utils.dvc import dvc_run
 from src.utils.helpers import filter_config
-from src.utils.vocab_creation import create_vocab
+from src.data_preprocessing.vocab_creation import create_vocab
 
 data_params = experiment_config.pop("data")
-dvc_run("src/utils/vocab_creation.py",
+dvc_run("src/data_preprocessing/vocab_creation.py",
         script_arguments=filter_config(data_params.as_dict(), create_vocab),
         dvc_file_name=f"{Path(__file__).stem}.dvc",
         input_deps=[data_params.get("train_qa_result_file"), data_params.get("val_qa_result_file")],
--- pipeline/preprocess_qa.py
+++ pipeline/preprocess_qa.py
@@ -5,6 +5,6 @@ from pipeline.init import experiment_config
 from src.utils.dvc import dvc_run
 
 data_params = experiment_config.pop("data")
-dvc_run("src/utils/load_qa.py",
+dvc_run("src/data_preprocessing/load_qa.py",
         dvc_file_name=f"{Path(__file__).stem}.dvc",
         output_deps=[data_params.get("train_qa_result_file"), data_params.get("val_qa_result_file")])
